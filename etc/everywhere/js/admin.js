4f33362e24e95ca0650fe3ec221fc8c78bae9ba4
Sander Mertens <sander.mertens8@gmail.com>
2016-02-16 02:08:49 -0500

    Browse objects

--- etc/everywhere/js/admin.js
+++ etc/everywhere/js/admin.js
@@ -16,22 +16,80 @@ corto.json = {
     return r + (pEnd || '');
     },
   prettyPrint: function(obj) {
-    var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-    return JSON.stringify(obj, null, 3)
-       .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-       .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-       .replace(jsonLine, corto.json.replacer);
+      if (obj != undefined) {
+        var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+        return JSON.stringify(obj, null, 3)
+           .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+           .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+           .replace(jsonLine, corto.json.replacer);
+      }
     }
   };
 
-$(function() {
-  
+// Compile templates
 var t_object = _.template($("#object").html());
 var t_objectList = _.template($("#objectList").html());
+var t_valueTree = _.template($("#valueTree").html());
 
-$.get("http://" + window.location.host + "/api?select=*&meta=true&value=true", function(data) {
+// Initialize parent to root
+corto.parent = "";
+
+// Translate identifier to link
+corto.link = function(ref, name) {
+  return "<a onclick=\"corto.request('" + ref + "')\">" +
+    name + "</a>";
+}
+
+corto.linkSplitUp = function(name) {
+  link = "";
+
+  // First add root
+  if (!name.length) {
+    result = corto.link("", "corto://");
+  } else {
+    result = corto.link("", "corto:/");
+  }
+
+  // Iterate over sections, add to link
+  _.each(name.split("/"), function(item) {
+    if (item.length) {
+      link += "/" + item;
+      result += "/<span class='object-id'>" + corto.link(link, item) + "</span>";
+    }
+  });
+
+  return result;
+}
+
+// Populate scope table
+corto.updateScope = function(data) {
   $("#scope").html(t_objectList({objects: data, objectTemplate: t_object}))
-  $("tr.object:even").css("background-color", "#23272e");
-});
+  $("#scope tr.object:even").css("background-color", "#23272e");
+}
+
+// Set parent
+corto.updateParent = function(id) {
+  $("#navigator").html(corto.linkSplitUp(id));
+}
+
+// Request a scope
+corto.request = function(id) {
+  corto.parent = id;
+  corto.updateScope([]);
+  corto.updateParent(id);
+  $.get("http://" + window.location.host +
+    "/api" + id + "?select=*&meta=true&value=true",
+    corto.updateScope);
+}
+
+// Document.ready
+$(function() {
+
+// Initialization of tables
+$("#scope").html(t_objectList({objects: [], objectTemplate: t_object}));
+$("#value").html(t_valueTree({}));
+
+// Initial request
+corto.request("");
 
 });
